7cc549a4c4677bde3b52f617236f53a2b5f08233
PriyankaMelgiri <159111241+PriyankaMelgiri@users.noreply.github.com>
2024-02-25 21:47:13 +0530

    Update Guest_Sign_Action.java

--- src/main/java/actions_Page/Guest_Sign_Action.java
+++ src/main/java/actions_Page/Guest_Sign_Action.java
@@ -36,6 +36,12 @@ public class Guest_Sign_Action {
 	public void signInUser() {
 		guestSignIn.signInUser.click();
 	}
+
+	public int add()
+	{
+		guestSignIn.signInUser.click();
+		int 20;
+	}
 	
 	
 }
